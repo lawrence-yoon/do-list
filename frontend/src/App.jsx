57cc91068ed4493e6dad168d024548a515133c6e
sadpandaemoji <sadpandaemoji@gmail.com>
2022-12-13 04:09:28 -0500

    style change

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -12,16 +12,16 @@ function App() {
 
 
   return (
-    <div className='bg-orange-300 h-screen'>
-      <header className='bg-orange-500 flex flex-row justify-between items-center h-10 p-5'>
+    <div className='bg-orange-300 h-full'>
+      <header className='bg-orange-500 flex flex-row justify-between items-center h-10 p-6'>
         <h1 className='text-4xl'>TaskBoard</h1>
-        <div className='flex gap-20'>
-          <a className='text-2xl'>Login</a>
+        <div className='flex justify-around gap-6'>
+          <a className='text-xl'>Login</a>
           {/* <a className='text-2xl'>Logout</a> */}
-          <a className='text-2xl'>Register</a>
+          <a className='text-xl'>Register</a>
         </div>
       </header>
-      <div className='flex flex-row items-start justify-center py-5 md:gap-3'>
+      <div className='flex flex-row items-start py-5 overflow-auto md:gap-3 md:justify-center'>
         <List list={listDo} setList={setListDo} setListLeft={setListDone} setListRight={setListDoing} label="✍️ Do List"/>
         <List list={listDoing} setList={setListDoing} setListLeft={setListDo} setListRight={setListDone} label="✍️ Doing List"/>
         <List list={listDone} setList={setListDone} setListLeft={setListDoing} setListRight={setListDo} label="✍️ Done List"/>
