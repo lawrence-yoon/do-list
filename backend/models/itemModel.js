46d5d533861f7e8adc6bda6940b2460582bc8640
sadpandaemoji <sadpandaemoji@gmail.com>
2022-12-14 06:21:08 -0500

    hook up basic API requests

---
+++ backend/models/itemModel.js
@@ -0,0 +1,25 @@
+const mongoose = require('mongoose');
+const itemSchema = mongoose.Schema({
+    // user: {
+    //     type: mongoose.Schema.Types.ObjectID,
+    //     required: true,
+    //     ref: 'User'
+    // },
+    title: {
+        type: String,
+        required: [true, 'Please add text']
+    },
+    details: {
+        type: String,
+        required: [true, 'Please add text']
+    },
+    list: {
+        type: String,
+        required: [true, 'Please add list']
+    }
+}, {
+    timestamps: true
+    }
+)
+
+module.exports = mongoose.model('Item', itemSchema)
--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,30 @@
+require('dotenv').config()
 const express = require("express")
 const app = express()
-const port = 3000
+const port = process.env.PORT || 3001
+const connectDB = require("./config/db")
+const Item = require('./models/itemModel')
+const mongoose = require('mongoose')
+
+connectDB()
+
+app.use(express.json())
+app.use(express.urlencoded())
 
 app.get('/', (req,res)=>{
     console.log("get request sent to '/'")
     res.send('root for backend taskboard')
+})
+
+app.post('/api/items', (req,res)=>{
+    console.log("post request sent to /api/items")
+    console.log(req.body.test)
+    const newItem = new Item({
+        title: req.body.title,
+        details: req.body.details,
+        list: req.body.list
+    })
+    res.status(200).json(newItem)
 })
 
 app.listen(port, ()=>{
