d8e5dcc32ea5f96b08d2199414080a85b6c155fe
sadpandaemoji <sadpandaemoji@gmail.com>
2022-12-14 08:04:43 -0500

    add login check, plain text password

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,7 @@ const app = express()
 const port = process.env.PORT || 3001
 const connectDB = require("./config/db")
 const Item = require('./models/itemModel')
+const User = require('./models/userModel')
 const mongoose = require('mongoose')
 
 connectDB()
@@ -16,6 +17,7 @@ app.get('/', (req,res)=>{
     res.send('root for backend taskboard')
 })
 
+//Create Item
 app.post('/api/items', (req,res)=>{
     console.log("post request sent to /api/items")
     console.log(req.body)
@@ -26,6 +28,45 @@ app.post('/api/items', (req,res)=>{
     })
     item.save()
     res.status(200).json(item)
+})
+//Create/Register User
+app.post('/api/users/register', (req,res)=>{
+    console.log("post request sent to /api/users/register")
+    console.log(req.body)
+    const user = new User({
+        name: req.body.name,
+        email: req.body.email,
+        password: req.body.password
+    })
+    user.save()
+    res.status(200).json(user)
+})
+
+//Login User
+app.post('/api/users/login', (req,res)=>{
+    console.log("post request sent to /api/users/login")
+    console.log(req.body)
+    const email = req.body.email
+    const password = req.body.password
+    User.findOne({email:email}, function(err, foundUser){
+        if (err) {
+            console.log(err)
+        }   else {
+            if(foundUser){
+                if(foundUser.password === password){
+                    console.log(`You are now logged in, ${foundUser.name}`)
+                    res.send(`You are now logged in, ${foundUser.name}`)
+                }   else{
+                    console.log('wrong password')
+                    res.send('wrong password')
+                }
+            }   else{
+                console.log("wrong user / user does not exist")
+                res.send("wrong user / user does not exist")
+            }
+        }
+    })
+    
 })
 
 app.listen(port, ()=>{
