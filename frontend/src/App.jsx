94ed82b5482a10b455a0fe32137d6201062887f7
sadpandaemoji <sadpandaemoji@gmail.com>
2022-12-26 13:54:47 -0500

    organize routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,9 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Header from "./components/Header";
 import TryMe from "./pages/TryMe";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import Dashboard from "./pages/Dashboard";
 
 function App() {
   return (
@@ -8,10 +11,10 @@ function App() {
       <Router>
         <Header />
         <Routes>
+          <Route path="/" element={<Dashboard />}></Route>
           <Route path="/tryme" element={<TryMe />}></Route>
-          <Route></Route>
-          <Route></Route>
-          <Route></Route>
+          <Route path="/login" element={<Login />}></Route>
+          <Route path="/register" element={<Register />}></Route>
         </Routes>
       </Router>
     </>
